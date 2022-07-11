976a246c6a4c9bf2b1504c2e0411eb9416613984
Josh Gen <64161160+jdg9vr@users.noreply.github.com>
2022-07-11 13:26:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='M14_HW_mods',
+setup(name='M14_HW',
       version='0.1',
       description='Homework file for Module 14',
       url='https://github.com/jdg9vr/M14_HW',
       author='Josh Gen',
       author_email='jdg9vr@virginia.edu',
       license='MIT',
-      packages=['M14_HW_mods'])
+      packages=['M14_HW'])
